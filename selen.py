05e936fe199a2cb4d79ee7e72ef9b09a8fbf8f80
Steven Buss <steven.buss@gmail.com>
2019-12-19 17:25:18 -0800

    Login and input individual details

---
+++ selen.py
@@ -0,0 +1,82 @@
+import csv
+import getpass
+import re
+
+from selenium.webdriver.common.keys import Keys
+
+
+def login(driver):
+    driver.get("https://netfile.com/Filer/Authentication/LogIn")
+
+    driver.find_element_by_id("UserName")
+    elem = driver.find_element_by_id("UserName")
+    elem.send_keys("steven.buss@gmail.com")
+    elem = driver.find_element_by_id("Password")
+    pw = getpass.getpass("password: ")
+    elem.send_keys(pw)
+    elem.send_keys(Keys.RETURN)
+
+
+def create_all_individuals(driver, contributions):
+    with open(contributions, 'r') as f:
+        reader = csv.DictReader(f)
+        names = set()
+        for row in reader:
+            name = row['Donor First Name'] + row['Donor Last Name']
+            if name in names:
+                continue
+            create_individual(driver, row)
+
+
+def create_individual(driver, person):
+    driver.get("https://netfile.com/Filer/LegacyFree/Entity/PeopleAdd?InProgressTransactionType=MonetaryContribution")  # NOQA
+
+    actblue_to_netfile = [
+        ('Donor First Name', 'FirstName_modal'),
+        ('Donor Last Name', 'LastName_modal'),
+        ('Donor City', 'BusinessAddress_City'),
+        ('Donor State', 'BusinessAddress_State'),
+        ('Donor ZIP', 'BusinessAddress_ZipCode'),
+        ('Donor Employer', 'Employer_modal'),
+        ('Donor Occupation', 'Occupation_modal'),
+        ('Donor Email', 'Email_modal'),
+    ]
+    for (person_col, elem_name) in actblue_to_netfile:
+        elem = driver.find_element_by_id(elem_name)
+        elem.send_keys(person[person_col])
+
+    # Address and phone are special case, because:
+    #  - Apt number is in BusinessAddress_Line2
+    #  - Phone is split across three fields
+    addr1, addr2 = person['Donor Addr1'], ""
+    pattern = re.compile(
+        r'(?P<addr1>.+)\s+(Apartment|Apt\.?|\#|Unit)\s?(?P<unit>.+)?',
+        re.IGNORECASE)
+
+    m = pattern.match(person['Donor Addr1'])
+    if m:
+        addr1 = m.groupdict()['addr1']
+        addr2 = m.groupdict()['unit']
+    else:
+        addr1 = person['Donor Addr1']
+    elem = driver.find_element_by_id('BusinessAddress_Line1')
+    elem.send_keys(addr1)
+    if addr2:
+        elem = driver.find_element_by_id('BusinessAddress_Line2')
+        elem.send_keys("Apt " + addr2)
+
+    if person['Donor Country'] == "United States":
+        phone = person['Donor Phone'].strip()
+        phone = re.sub(r'[^\d]', '', person['Donor Phone'])
+        if phone:
+            pattern = re.compile(
+                r'1?(?P<area>\d{3})(?P<exchange>\d{3})(?P<number>\d{4})')
+            m = pattern.match(phone)
+            if m:
+                elem = driver.find_element_by_id('WorkPhone_AreaCode')
+                elem.send_keys(m.groupdict()['area'])
+                elem = driver.find_element_by_id('WorkPhone_Exchange')
+                elem.send_keys(m.groupdict()['exchange'])
+                elem = driver.find_element_by_id('WorkPhone_Number')
+                elem.send_keys(m.groupdict()['number'])
+    elem.send_keys(Keys.RETURN)
